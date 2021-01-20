fd277a18355dbd14a62dc4d96a80255eefed189f
Ramon <ramondavi@msn.com>
2021-01-20 15:06:44 -0300

    graph changes

--- index.js
+++ index.js
@@ -129,8 +129,8 @@ window.onload = function () {
             },
             legend: {
                 display: true,
-                position: 'right',
-                align: 'end',
+                position: 'bottom',
+                align: 'center'
             }
         }
     });
@@ -140,7 +140,7 @@ window.onload = function () {
         type: 'polarArea',
         data: {
             labels: [
-                        'Objetividade e clareza',
+                        'Objetividade, clareza e transparência',
                         'Domínio storytelling',
                         'Visibilidade',
                         'Cultura de feedback',
@@ -186,8 +186,8 @@ window.onload = function () {
             },
             legend: {
                 display: true,
-                position: 'right',
-                align: 'end',
+                position: 'bottom',
+                align: 'center'
             }
         }
     });
@@ -197,16 +197,10 @@ window.onload = function () {
         type: 'polarArea',
         data: {
             labels: [
-                
-                    
                         'Cocriação',
-
                         'Ser propositivo',
-
                         'Auto desenvolvimento',
-
                         'Impacto no negócio',
-
                         'Auto gestão',
 
             ],
@@ -239,16 +233,24 @@ window.onload = function () {
         },
         options: {
             scale: {
+                scaleLabel: {
+                    display: true
+                  },
                 ticks: {
                     suggestedMin: 0,
                     suggestedMax: 5,
                     stepSize: 1
-                }
+                },
+                angleLines: {
+                    display: true,
+                  },
+                  pointLabels: {
+                    display: true,
+                    fontSize: 12
+                  }
             },
             legend: {
-                display: true,
-                position: 'right',
-                align: 'end',
+                display: false
             }
         }
     });
@@ -270,7 +272,7 @@ window.onload = function () {
                             '3',
                             '3', 
                 ],
-                fill: true,
+                fill: false,
                 backgroundColor: [
                             'rgba(203, 163, 59, 0.5)',
                             'rgba(255, 230, 0, 0.5)',
@@ -287,19 +289,414 @@ window.onload = function () {
             }]
         },
         options: {
+            
             scale: {
+                scaleLabel: {
+                    display: true
+                  },
                 ticks: {
                     suggestedMin: 0,
                     suggestedMax: 5,
                     stepSize: 1
-                }
+                },
+                angleLines: {
+                    display: true,
+                  },
+                  pointLabels: {
+                    display: false,
+                    fontSize: 12
+                  }
             },
             legend: {
-                display: true,
-                position: 'right',
-                align: 'end',
+                display: false
             }
         }
     });
-
+    
+    
 }
+
+let myChart = new Chart(document.getElementById("pie-chart"), {
+    type: "polarArea",
+    data: {
+      datasets: [{
+        data: [2, 4, 3, 3],
+        backgroundColor: ["rgba(203, 163, 59, 0.5)", "rgba(255, 230, 0, 0.5)", "rgba(255, 230, 0, 0.5)", "rgba(203, 163, 59, 0.5)"],
+        borderWidth: 1,
+        borderColor: [
+                              'rgba(203, 163, 59, 1)',
+                              'rgba(255, 230, 0, 1)',
+                              'rgba(255, 230, 0, 1)',
+                              'rgba(203, 163, 59, 1)',
+                  ]
+      }],
+      labels: [
+        "Flexibilidade",
+        "Negociação",
+        "Evolui e constrói a cultura",
+        "Mediação"
+      ],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        datalabels: {
+          formatter: function(value, context) {
+            return context.chart.data.labels[context.dataIndex];
+          },
+          anchor: 'start',
+          align: 'end',
+          offset: 0 // Gets updated
+        },
+      },
+      cutoutPercentage: 20,
+      legend: {
+        display: false
+      },
+      layout: {
+        padding: 120,
+      },
+      scale: {
+        scaleLabel: {
+          display: true
+        },
+        ticks: {
+          suggestedMin: 0,
+          suggestedMax: 5,
+          stepSize: 1
+        },
+        angleLines: {
+          display: true
+        },
+        pointLabels: {
+          display: false
+        }
+      },
+      onResize: function() {
+        let width = document.getElementById("pie-chart").width
+        let padding = myChart.options.layout.padding
+        myChart.options.plugins.datalabels.offset = width/2-padding
+      }
+    }
+  });
+  
+  function updateOffset() {
+    let width = document.getElementById("pie-chart").width
+    let padding = myChart.options.layout.padding
+    myChart.options.plugins.datalabels.offset = width/2-padding
+    myChart.update()
+  }
+  updateOffset() //init call after we know the dimensions of the chart
+  
+
+
+  let myChart2 = new Chart(document.getElementById("pie-chart2"), {
+    type: "polarArea",
+    data: {
+      datasets: [{
+        data: ['5','1','4','4','3',],
+        backgroundColor: [
+            'rgba(255, 170, 221, 0.5)',
+            'rgba(255, 170, 221, 0.5)',
+            'rgba(243, 99, 186, 0.5)',
+            'rgba(243, 99, 186, 0.5)',
+            'rgba(203, 57, 144, 0.5)'],
+        borderWidth: 1,
+        borderColor: [
+            'rgba(255, 170, 221, 1)',
+            'rgba(255, 170, 221, 1)',
+            'rgba(243, 99, 186, 1)',
+            'rgba(243, 99, 186, 1)',
+            'rgba(203, 57, 144, 1)',
+                  ]
+      }],
+      labels: [
+        'Cocriação',
+        'Ser propositivo',
+        'Auto desenvolvimento',
+        'Impacto no negócio',
+        'Auto gestão'
+      ],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        datalabels: {
+          formatter: function(value, context) {
+            return context.chart.data.labels[context.dataIndex];
+          },
+          anchor: 'start',
+          align: 'end',
+          offset: 0 // Gets updated
+        },
+      },
+      cutoutPercentage: 20,
+      legend: {
+        display: false
+      },
+      layout: {
+        padding: 120,
+      },
+      scale: {
+        scaleLabel: {
+          display: true
+        },
+        ticks: {
+          suggestedMin: 0,
+          suggestedMax: 5,
+          stepSize: 1
+        },
+        angleLines: {
+          display: true
+        },
+        pointLabels: {
+          display: false
+        }
+      },
+      onResize: function() {
+        let width = document.getElementById("pie-chart2").width
+        let padding = myChart2.options.layout.padding
+        myChart2.options.plugins.datalabels.offset = width/2-padding
+      }
+    }
+  });
+  
+  function updateOffset2() {
+    let width = document.getElementById("pie-chart2").width
+    let padding = myChart2.options.layout.padding
+    myChart2.options.plugins.datalabels.offset = width/2-padding
+    myChart2.update()
+  }
+  updateOffset2() //init call after we know the dimensions of the chart
+
+
+
+
+
+
+  let myChart3 = new Chart(document.getElementById("pie-chart3"), {
+    type: "polarArea",
+    data: {
+      datasets: [{
+        data: ['1','2','5','5'],
+        backgroundColor: [
+            'rgba(139, 237, 178, 0.5)',
+            'rgba(67, 200, 200, 0.5)',
+            'rgba(67, 200, 200, 0.5)',
+            'rgba(139, 237, 178, 0.5)'],
+        borderWidth: 1,
+        borderColor: [
+            'rgba(139, 237, 178, 1)',
+            'rgba(67, 200, 200, 1)',
+            'rgba(67, 200, 200, 1)',
+            'rgba(139, 237, 178, 1)'
+                  ]
+      }],
+      labels: [
+        'Objetividade, clareza e  \n transparência',
+        'Domínio storytelling',
+        'Visibilidade',
+        'Cultura de feedback',
+      ],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        datalabels: {
+          formatter: function(value, context) {
+            return context.chart.data.labels[context.dataIndex];
+          },
+          anchor: 'start',
+          align: 'end',
+          offset: 0 // Gets updated
+        },
+      },
+      cutoutPercentage: 20,
+      legend: {
+        display: false
+      },
+      layout: {
+        padding: 120,
+      },
+      scale: {
+        scaleLabel: {
+          display: true
+        },
+        ticks: {
+          suggestedMin: 0,
+          suggestedMax: 5,
+          stepSize: 1
+        },
+        angleLines: {
+          display: true
+        },
+        pointLabels: {
+          display: false
+        }
+      },
+      onResize: function() {
+        let width = document.getElementById("pie-chart3").width
+        let padding = myChart3.options.layout.padding
+        myChart3.options.plugins.datalabels.offset = width/2-padding
+      }
+    }
+  });
+  
+  function updateOffset3() {
+    let width = document.getElementById("pie-chart3").width
+    let padding = myChart3.options.layout.padding
+    myChart3.options.plugins.datalabels.offset = width/2-padding
+    myChart3.update()
+  }
+  updateOffset3() //init call after we know the dimensions of the chart
+  
+  
+  let myChart4 = new Chart(document.getElementById("pie-chart4"), {
+    type: "polarArea",
+    data: {
+      datasets: [{
+        data: ['3','4','0','1'],
+        backgroundColor: [
+            'rgba(193, 61, 255, 0.5)',
+            'rgba(224, 158, 255, 0.5)',
+            'rgba(224, 158, 255, 0.5)',
+            'rgba(193, 61, 255, 0.5)',],
+        borderWidth: 1,
+        borderColor: [
+            'rgba(193, 61, 255, 1)',
+            'rgba(224, 158, 255, 1)',
+            'rgba(224, 158, 255, 1)',
+            'rgba(193, 61, 255, 1)'
+                  ]
+      }],
+      labels: [
+        'Qualidade',
+        'Frequência',
+        'Adaptação ao processo',
+        'Experimentação',
+      ],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        datalabels: {
+          formatter: function(value, context) {
+            return context.chart.data.labels[context.dataIndex];
+          },
+          anchor: 'start',
+          align: 'end',
+          offset: 0 // Gets updated
+        },
+      },
+      cutoutPercentage: 20,
+      legend: {
+        display: false
+      },
+      layout: {
+        padding: 120,
+      },
+      scale: {
+        scaleLabel: {
+          display: true
+        },
+        ticks: {
+          suggestedMin: 0,
+          suggestedMax: 5,
+          stepSize: 1
+        },
+        angleLines: {
+          display: true
+        },
+        pointLabels: {
+          display: false
+        }
+      },
+      onResize: function() {
+        let width = document.getElementById("pie-chart4").width
+        let padding = myChart4.options.layout.padding
+        myChart4.options.plugins.datalabels.offset = width/2-padding
+      }
+    }
+  });
+  
+  function updateOffset4() {
+    let width = document.getElementById("pie-chart4").width
+    let padding = myChart4.options.layout.padding
+    myChart4.options.plugins.datalabels.offset = width/2-padding
+    myChart4.update()
+  }
+  updateOffset4() //init call after we know the dimensions of the chart
+
+
+
+  let myChart5 = new Chart(document.getElementById("pie-chart5"), {
+    type: "polarArea",
+    data: {
+      datasets: [{
+        data: ['2','3','3','3'],
+        backgroundColor: [
+            'rgba(193, 61, 255, 0.5)',
+            'rgba(67, 200, 200, 0.5)',
+            'rgba(243, 99, 186, 0.5)',
+            'rgba(255, 230, 0, 0.5)'
+        ],
+        borderWidth: 1,
+        borderColor: [
+            'rgba(193, 61, 255, 0.5)',
+            'rgba(67, 200, 200, 0.5)',
+            'rgba(243, 99, 186, 0.5)',
+            'rgba(255, 230, 0, 0.5)'
+                  ]
+      }],
+      labels: ['Execução', 'Comunicação', 'Propriedade', 'Influência'],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        datalabels: {
+          formatter: function(value, context) {
+            return context.chart.data.labels[context.dataIndex];
+          },
+          anchor: 'start',
+          align: 'end',
+          offset: 0 // Gets updated
+        },
+      },
+      cutoutPercentage: 20,
+      legend: {
+        display: false
+      },
+      layout: {
+        padding: 120,
+      },
+      scale: {
+        scaleLabel: {
+          display: true
+        },
+        ticks: {
+          suggestedMin: 0,
+          suggestedMax: 5,
+          stepSize: 1
+        },
+        angleLines: {
+          display: true
+        },
+        pointLabels: {
+          display: false
+        }
+      },
+      onResize: function() {
+        let width = document.getElementById("pie-chart5").width
+        let padding = myChart5.options.layout.padding
+        myChart5.options.plugins.datalabels.offset = width/2-padding
+      }
+    }
+  });
+  
+  function updateOffset5() {
+    let width = document.getElementById("pie-chart5").width
+    let padding = myChart5.options.layout.padding
+    myChart5.options.plugins.datalabels.offset = width/2-padding
+    myChart5.update()
+  }
+  updateOffset5() //init call after we know the dimensions of the chart
